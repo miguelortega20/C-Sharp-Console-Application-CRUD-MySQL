303a8fabc47dee06894cf45ac26cf49e15322a15
Miguel Angel Ortega Flores <miguelortega.20@outlook.com>
2019-10-01 22:06:19 -0500

    conection mysql

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace CRUD1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C#: Connect to MySQL");
+
+            try{
+
+                string connectionString;
+                connectionString = "server=localhost;port=3306;uid=root;pwd='';database=crud;charset=utf8;SslMode=none;";
+                MySqlConnection con = new MySqlConnection(connectionString);
+
+                con.Open();
+                Console.WriteLine("Conection is " + con.State.ToString() + Environment.NewLine);
+
+                con.Close();
+                Console.WriteLine("Conection is " + con.State.ToString() + Environment.NewLine);
+
+            }catch(MySql.Data.MySqlClient.MySqlException ex){
+                Console.Write("Error: " + ex.Message.ToString());
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.Read();
+        }
+    }
+}
